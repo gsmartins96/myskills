a84c4bccdf5774ad5aa524a88895fd4a1fe8dc1e
Gabriel Martins <gsmartins96@hotmail.com>
2022-01-27 22:16:27 -0300

    Sentry configuration for Error Tracking in Production

--- App.tsx
+++ App.tsx
@@ -2,6 +2,12 @@ import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { Home } from './src/pages/Home';
 import SplashScreen from 'react-native-splash-screen';
+import * as Sentry from "@sentry/react-native";
+
+Sentry.init({
+  dsn: "https://e0cb4fc767b04856b0eec49bec91b84a@o336309.ingest.sentry.io/6172012",
+  tracesSampleRate: 1.0,
+});
 
 export default function App(){
   useEffect(() => {
